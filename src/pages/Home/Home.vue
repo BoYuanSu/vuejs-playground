8f9d8701efeefa788726fc45401b78f7da9c369d
BYSu <boyuansu@callcarbar.com.tw>
2021-04-21 18:13:57 +0800

    create home page demo portfolios

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="home container">
+    <div class="user-info">
+      <div class="user-info-avatar">
+        <img
+          src="@/assets/image/avatar.jpg"
+          alt=""
+        >
+      </div>
+      <div class="user-info-name">
+        <router-link
+          data-no-turbolink="true"
+          :to="{name: 'Home'}"
+        >
+          Bo Yuan Su
+        </router-link>
+      </div>
+    </div>
+    <div class="portfolios">
+      <div
+        v-for="portfolio in portfolios"
+        :key="portfolio.name"
+        class="portfolio"
+      >
+        <div class="portfolio-wrapper">
+          <div class="portfolio-img-header">
+            <img
+              :src="portfolio.img"
+              :alt="portfolio.name"
+            >
+          </div>
+          <div class="portfolio-text">
+            <p>{{ portfolio.name }}</p>
+          </div>
+          <div class="portfolio-link">
+            <router-link :to="portfolio.link">
+              前往
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      portfolios: [
+        {
+          img: 'https://picsum.photos/id/684/600/400',
+          name: 'OhBot 行事曆',
+          link: { name: 'OhBot' }
+        },
+        {
+          img: 'https://picsum.photos/id/684/600/400',
+          name: 'OhBot 行事曆1',
+          link: { name: 'OhBot' }
+        },
+        {
+          img: 'https://picsum.photos/id/684/600/400',
+          name: 'OhBot 行事曆2',
+          link: { name: 'OhBot' }
+        },
+        {
+          img: 'https://picsum.photos/id/684/600/400',
+          name: 'OhBot 行事曆2',
+          link: { name: 'OhBot' }
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import OhBot from '../pages/OhBot'
+import Home from '../pages/Home/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/oh-bot',
     name: 'OhBot',
-    component: OhBot
+    component: () => import(/* webpackChunkName: "oh-bot" */ '../pages/OhBot')
+  },
+  {
+    path: '*',
+    redirect: 'Home'
   }
 ]
 
